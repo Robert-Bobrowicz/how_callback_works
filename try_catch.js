3d41618b6f704be49561c84cecf490e821ae1f2f
unknown <r.bobrowicz@live.com>
2022-06-15 21:32:08 +0200

    promises

---
+++ try_catch.js
@@ -0,0 +1,32 @@
+// async function and try catch example - how to replace Promise()
+
+const flightData = {
+    from: "Berlin",
+    destination: "London",
+    flight: 'LH3467',
+    departure: new Date(2022, 5, 16, 12, 45)   //try to change to new Date(2021,5,16,12,45)
+};
+
+const today = new Date();
+
+const myFlight = () => {
+    if (flightData.departure.getTime() > today.getTime()) {
+        flightData.departure = flightData.departure.toLocaleString();
+        console.log(`Your flight data to ${flightData.destination} will depart on: `);
+        return flightData;
+    } else {
+        throw `We are sorry to inform you that your plane to ${flightData.destination} departed on ${flightData.departure.toDateString()}.`;
+    }
+}
+
+
+const asyncFunction = async () => {
+    try {
+        const success = myFlight();
+        console.log(success);
+    } catch (error) {
+        console.log('Information for a passanger: ', error);
+    }
+}
+
+asyncFunction();
